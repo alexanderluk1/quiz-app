74afd49c64b14a570302f8b4113139679f7ae46b
Alexander <luk.weiheng@gmail.com>
2023-12-14 20:55:05 +0800

    Amended the correct questions counter to using every()

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 const formSelected = document.querySelector("#quiz-form");
 const alertSelected = document.querySelector("#alert");
 
-// # of Correct Question
-let numCorrectQuestions = 0;
-
 formSelected.addEventListener("submit", (e) => {
   // Prevent the page from refreshing after submit
   e.preventDefault();
@@ -15,8 +12,10 @@ formSelected.addEventListener("submit", (e) => {
   // Loop through each question
   convertedQuestions.forEach((eachQuestion) => {
     const allAnswers = eachQuestion.querySelectorAll(".answer");
+
     // Firstly, set the answer to incorrect
     eachQuestion.classList.add("incorrect");
+    eachQuestion.correctAnswer = false;
 
     // Loop through each question
     allAnswers.forEach((eachAns) => {
@@ -26,20 +25,23 @@ formSelected.addEventListener("submit", (e) => {
         eachQuestion.classList.remove("incorrect");
         eachQuestion.classList.add("correct");
 
-        // Increment number of correct answers
-        numCorrectQuestions++;
+        // Set this question to correct
+        eachQuestion.correctAnswer = true;
         return;
       }
       // If there is no correct answer picked, class will remain as incorrect
     });
   });
 
+  // Check if all questions are correct
+  const allCorrect = convertedQuestions.every((eachQn) => {
+    return eachQn.correctAnswer;
+  });
+
   // If the user gets all question correct, this will run
-  if (numCorrectQuestions === convertedQuestions.length) {
+  if (allCorrect) {
     // Display Congratulation
     alertSelected.classList.add("active");
-    // Reset the correct counter to 0
-    numCorrectQuestions = 0;
     // Remove Congratulation after 1 second
     setTimeout(() => {
       alertSelected.classList.remove("active");
